c46439ddf167dd9482c6299680e601ad25171efa
Nut Janekitiworapong <nutjane131@gmail.com>
2018-06-10 15:56:39 +0700

    swagger info

--- src/main/java/com/example/sample/controller/GreetingController.java
+++ src/main/java/com/example/sample/controller/GreetingController.java
@@ -3,6 +3,7 @@ package com.example.sample.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.example.sample.model.Greeting;
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@Api(value = "test", description = "this is a test api to check service health")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
