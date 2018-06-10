71fdee806f6e5eaeda2231060e077aad9f72a7e3
Nut Janekitiworapong <nutjane131@gmail.com>
2018-06-10 17:24:04 +0700

    add new user to mysql

--- sql/dummy.sql
+++ sql/dummy.sql
@@ -1,3 +1,6 @@
+create user 'springuser'@'%' identified by 'ThePassword'; -- Creates the user
+grant all on TestDB.* to 'springuser'@'%'; -- Gives all the privileges to the new user on the newly created database
+
 CREATE TABLE Persons (
     PersonID int,
     LastName varchar(255),
