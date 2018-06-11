69555c86bb9d9d30d035686262581d04b0c703b9
Nut Janekitiworapong <nutjane131@gmail.com>
2018-06-11 23:53:39 +0700

    access sql data

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ## how to run
 
 `./parepare.sh` to start database docker
+
+## view
+
+```
+ocalhost:8080/swagger-ui.html       for swagger ui
+
+```
---
+++ src/main/java/com/example/sample/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.sample.repository;
+
+import com.example.sample.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByName(String name);
+}
